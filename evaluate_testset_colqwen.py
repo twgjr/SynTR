4f4839cc763760c5d263fb0dc50dec4a3324110d
twgjr <terrwaynegreen@gmail.com>
2025-04-12 23:12:34 -0400

    evaluate working with finetuned model

--- evaluate_testset_colqwen.py
+++ evaluate_testset_colqwen.py
@@ -9,22 +9,6 @@ from peft import PeftModel
 from vilarmor_dataset import ViLARMoRDataset
 from evaluator import ViLARMoREvaluator
 
-def load_test_dataset():
-    data_files = {"test": "beir_splits/test.jsonl"}
-    beir_dataset = load_dataset("json", data_files=data_files)
-
-    dataset_name = "vidore/docvqa_test_subsampled_beir"
-    vil_dataset = ViLARMoRDataset(name=dataset_name, load_pseudos=True, load_judgements=True)
-
-    raw_corpus = list(vil_dataset.corpus)  # Make sure it's a list
-    corpus_dataset = Dataset.from_list(raw_corpus)
-
-    # Rename 'corpus-id' to 'docid' as expected by collator
-    if "corpus-id" in corpus_dataset.column_names:
-        corpus_dataset = corpus_dataset.rename_column("corpus-id", "docid")
-
-    return beir_dataset["test"], corpus_dataset, "vidore"
-
 def main():
     bnb_config = BitsAndBytesConfig(load_in_8bit=True)
 
@@ -38,38 +22,37 @@ def main():
 
     # Load adapter weights
     model = PeftModel.from_pretrained(base_model, "./colqwen_beir_checkpoints/checkpoint-21")
+
+    from types import MethodType
+
+    ##################################################################
+    # Monkey-patch inner_forward to safely remove 'labels' if present
+    original_forward = model.base_model.forward
+
+    def safe_forward(self, *args, **kwargs):
+        if "labels" in kwargs:
+            kwargs.pop("labels")
+        return original_forward(*args, **kwargs)
+
+    from types import MethodType
+    model.base_model.forward = MethodType(safe_forward, model.base_model)
+    # End monkey patch
+    #################################################################
     
-    # from types import MethodType
-
-    # # Access the true inner model inside PEFT wrapper
-    # inner_model = model.base_model.model
-    # original_inner_forward = inner_model.inner_forward
-
-    # def safe_inner_forward(self, *args, **kwargs):
-    #     if "labels" in kwargs:
-    #         kwargs.pop("labels")
-    #     return original_inner_forward(*args, **kwargs)
-
-    # # Patch the true method
-    # inner_model.inner_forward = MethodType(safe_inner_forward, inner_model)
-
     processor = ColQwen2_5_Processor.from_pretrained(
         "Metric-AI/colqwen2.5-3b-multilingual",
         use_fast=False
     )
-
-    test_set, corpus_dataset, corpus_format = load_test_dataset()
-
 
     DATASETS = [
         "vidore/docvqa_test_subsampled_beir",
     ]
 
     MODELS = {
+        "colqwen_finetuned": [model, processor], # contains instances
         "Metric-AI/ColQwen2.5-3b-multilingual-v1.0": [ColQwen2_5, ColQwen2_5_Processor],
         "Metric-AI/colqwen2.5-3b-multilingual": [ColQwen2_5, ColQwen2_5_Processor],
         "vidore/colqwen2.5-v0.2": [ColQwen2_5, ColQwen2_5_Processor],
-        "colqwen_finetuned": [model, processor] # contains instances
     }
 
 
